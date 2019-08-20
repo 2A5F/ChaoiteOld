8bb80586081333dba303a4cf92c7840ce65df3c8
2A5F <Aditrc@outlook.com>
2019-08-20 13:54:51 +0800

    save

---
+++ Utils/Utils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeowType.Chaoite.Utils
+{
+    public static class Utils
+    {
+        public static IEnumerable<T> PopFor<T>(this Stack<T> self)
+        {
+            while (self.TryPop(out var outv))
+            {
+                yield return outv;
+            }
+        }
+    }
+}
